ca301ce94ce03bf62472634f4d7ac4334fd7f872
briantcu <bri.reg@gmail.com>
2014-04-26 12:29:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qfit::Application.config.secret_key_base = 'ac52587cf7071cffaa4187f6d673d419b8c40b3c2b7a3c1f016f06ae402f0ee971fc67dc30a34bea339f683ff895febc0c0c26814b57ee39aa49215343a50571'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Qfit::Application.config.session_store :cookie_store, key: '_qfit_session'
