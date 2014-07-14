bce5bec4b78d4ab86ca952955c6458d3483faf0d
briantcu <bri.reg@gmail.com>
2014-07-14 17:20:30 -0500

    Adding group performed warmup

--- app/models/group_performed_warmup.rb
+++ app/models/group_performed_warmup.rb
@@ -12,4 +12,9 @@
 
 class GroupPerformedWarmup < ActiveRecord::Base
   belongs_to :group_routine
+
+
+  def self.add_exercise(exercise_id, status, routine_id)
+      GroupPerformedWarmup.create(routine_id: routine_id, warmup_id: exercise_id, status: status)
+  end
 end
--- test/models/group_performed_warmup_test.rb
+++ test/models/group_performed_warmup_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class GroupPerformedWarmupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'adds exercise' do
+    exercise = GroupPerformedWarmup.add_exercise(3, 2, 1)
+    assert(exercise != nil)
+  end
 end
