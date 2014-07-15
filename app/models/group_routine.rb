0ea0466d391cad3b8690da3cf93d29b004f640de
briantcu <bri.reg@gmail.com>
2014-07-14 21:01:32 -0500

    Group routine changes

--- app/models/group_routine.rb
+++ app/models/group_routine.rb
@@ -21,10 +21,97 @@
 #
 
 class GroupRoutine < ActiveRecord::Base
+
+  STRETCHING = 4
+  WEIGHTS = 1
+  PLYOS = 2
+  SPRINTING = 3
+
+
   belongs_to :group
   has_many :group_performed_exercises, -> { order('id ASC') }, :foreign_key => :routine_id
   has_many :group_performed_plyos, -> { order('id ASC') }, :foreign_key => :routine_id
   has_many :group_performed_sprints, -> { order('id ASC') }, :foreign_key => :routine_id
   has_many :group_performed_warmups, -> { order('id ASC') }, :foreign_key => :routine_id
   has_many :group_custom_exercises, -> { order('id ASC') }, :foreign_key => :routine_id
+
+
+  def self.get_matching_routine_since(date, type, day_id, group_id)
+    matching = nil
+    case type
+      when STRETCHING
+        matching = GroupRoutine.where(:group_id => group_id).where('day_performed > ?', date).where(:wu_day_id => day_id).order(day_performed: :desc).first
+      when WEIGHTS
+        matching = GroupRoutine.where(:group_id => group_id).where('day_performed > ?', date).where(:wt_day_id => day_id).order(day_performed: :desc).first
+      when PLYOS
+        matching = GroupRoutine.where(:group_id => group_id).where('day_performed > ?', date).where(:pl_day_id => day_id).order(day_performed: :desc).first
+      when SPRINTING
+        matching = GroupRoutine.where(:group_id => group_id).where('day_performed > ?', date).where(:sp_day_id => day_id).order(day_performed: :desc).first
+    end
+    matching
+  end
+
+  def note_warmup_changes_saved
+    self.changes_saved = true
+    self.wu_modified = true
+    self.save
+    self.group.users.each do |user|
+      user_routine = DailyRoutine.get_routine_from_group_routine_id(self.id, self.group.id, user.id)
+      if !user_routine.nil?
+        user_routine.note_warmup_changes_saved
+      end
+    end
+  end
+
+  def get_warmups_without_changes_saved
+    self.group_performed_warmups.where('status == 2 or status == 3').order(id: :asc)
+  end
+
+  def add_warmup(exercise_id, status, not_used)
+    exercise = GroupPerformedWarmup.add_exercise(exercise_id, status, self.id)
+    add_for_users(PerformedWarmUp, exercise.id)
+    exercise
+  end
+
+  def note_day_created(day_id, type)
+    case type
+      when STRETCHING
+        self.wu_day_id = day_id
+      when WEIGHTS
+        self.wt_day_id = day_id
+      when PLYOS
+        self.pl_day_id = day_id
+      when SPRINTING
+        self.sp_day_id = day_id
+    end
+    self.save
+    self.group.users.each do |user|
+      user_routine = DailyRoutine.get_routine_from_group_routine_id(self.id, self.group.id, user.id)
+      if !user_routine.nil?
+        user_routine.note_day_created(day_id, type)
+      end
+    end
+  end
+
+  def add_custom_exercise(name, type, not_used)
+    group_exercise = GroupCustomExercise.add_exercise(self.id, name, type)
+    self.group.users.each do |user|
+      user_routine = DailyRoutine.get_routine_from_group_routine_id(self.id, self.group.id, user.id)
+      if !user_routine.nil?
+        user_routine.add_custom_exercise(name, type, group_exercise.id)
+      end
+    end
+  end
+
+  private
+
+  def add_for_users(performed_entity, performed_id)
+    self.group.users.each do |user|
+      user_routine = DailyRoutine.get_routine_from_group_routine_id(self.id, self.group.id, user.id)
+      if !user_routine.nil?
+        performed_entity.add_exercise(exercise_id, status, user_routine.id, performed_id)
+      end
+    end
+  end
+
 end
--- test/models/group_routine_test.rb
+++ test/models/group_routine_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class GroupRoutineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'gets warmups without changes saved' do
+    routine = GroupRoutine.find(1)
+    routine.note_warmup_changes_saved
+    assert(routine.changes_saved == true)
+    assert(routine.wu_modified == true)
+  end
+
+  test 'adds a warm up' do
+    routine = GroupRoutine.find(1)
+    exercise = routine.add_warmup(2, 2, 0)
+    assert(exercise != nil)
+  end
+
+  test 'notes day created' do
+    routine = GroupRoutine.find(1)
+    routine.note_day_created(3, 1)
+    assert(routine.wt_day_id == 3)
+
+    routine.note_day_created(4, 2)
+    assert(routine.pl_day_id == 4)
+
+    routine.note_day_created(2, 3)
+    assert(routine.sp_day_id == 2)
+
+    routine.note_day_created(5, 4)
+    assert(routine.wu_day_id == 5)
+  end
+
+  test 'adds custom exercise' do
+    routine = GroupRoutine.find(1)
+    routine.add_custom_exercise('custom', 2, '')
+    assert(routine.group_custom_exercises.last.name == 'custom')
+  end
 end
