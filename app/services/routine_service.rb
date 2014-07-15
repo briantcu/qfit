d7ed7c9a1503cdae206a4eb0cd0f5f44f92076f1
briantcu <bri.reg@gmail.com>
2014-07-14 21:33:28 -0500

    Routine service generating warm ups

--- app/services/routine_service.rb
+++ app/services/routine_service.rb
@@ -1,16 +1,23 @@
 class RoutineService
 
+  STRETCHING = 4
+  WEIGHTS = 1
+  PLYOS = 2
+  SPRINTING = 3
+
   @date
-  @entity
+  @entity #Either a user or group
   @routine
   @phase_number # 1-4
+  @sched_update
   @cause_of_new_routine #Cron job to create new workouts, or schedule/goals change, or new member/group creation,
                         # or member removed from group. Cron, Sched, New, Removed
 
-  def initialize(entity, cause_of_new_routine, date)
+  def initialize(entity, cause_of_new_routine, date, sched_update)
     @entity = entity
     @cause_of_new_routine = cause_of_new_routine
     @date = date
+    @sched_update = sched_update
   end
 
   def create_routine
@@ -25,45 +32,92 @@ class RoutineService
       weekly_schedule_day = @entity.get_schedule.get_schedule_day(@date)
 
       if weekly_schedule_day.stretching
-        add_stretching
+        add_exercises(WarmupService, STRETCHING)
       end
 
       if weekly_schedule_day.weights
-        add_weights
+        add_exercises(WeightsService, WEIGHTS)
       end
 
       if weekly_schedule_day.plyometrics
-        add_plyos
+        add_exercises(PlyometricsService, PLYOS)
       end
 
       if weekly_schedule_day.sprinting
-        add_sprinting
+        add_exercises(SprintingService, SPRINTING)
       end
 
-      maybe_add_custom_exercises
-
       cleanup
+
+      maybe_add_custom_exercises
 
       @routine
     end
   end
 
+  def get_next_day_id(type)
+    next_day = 0
+    case type
+      when STRETCHING
+        next_day = @entity.last_warmup_day_created
+      when WEIGHTS
+        next_day = @entity.last_weight_day_created
+      when PLYOS
+        next_day = @entity.last_plyometric_day_created
+      when SPRINTING
+        next_day = @entity.last_sprint_day_created
+    end
+
+    if !@sched_update || (next_day == 0)
+      total_days = @entity.get_schedule.get_total_days_of_pillar(type)
+      next_day = next_day + 1
+      if next_day > total_days
+        next_day = 1
+      end
+    end
+    next_day
+
+  end
+
+  def get_previous_matching_routine(type, day_id)
+    temp_date = @date - 15
+    if @entity.is_group
+      prev_routine  = GroupRoutine.get_matching_routine_since(temp_date, type, day_id, @entity.id)
+    else
+      prev_routine  = DailyRoutine.get_matching_routine_since(temp_date, type, day_id, @entity.id)
+    end
+    validate_routine_matches(prev_routine)
+  end
+
   private
 
-  def add_stretching
-
+  def add_exercises(service, type)
+    day_id = self.get_next_day_id(type)
+    previous_routine = self.get_previous_matching_routine(type, day_id)
+    if previous_routine.nil?
+      service.add_exercises(@entity, @routine)
+    else
+      service.copy_exercises(@entity, @routine, previous_routine)
+    end
+    @entity.note_last_day_created(day_id, type)
+    @routine.note_day_created(day_id, type)
   end
 
-  def add_weights
+  def validate_routine_matches(prev_routine)
+    if prev_routine.nil?
+      return nil
+    end
 
-  end
+    prev_phase = @entity.get_schedule.get_phase_by_date(prev_routine.day_performed)
+    if prev_phase != @phase_number
+      return nil
+    end
 
-  def add_plyos
+    if prev_routine.id == @routine.id
+      return nil
+    end
 
-  end
-
-  def add_sprinting
-
+    prev_routine
   end
 
   def maybe_add_custom_exercises
@@ -72,7 +126,7 @@ class RoutineService
 
   def valid_entity_and_open_date?
     #Check for the presence of entity and a schedule
-    valid = (@entity != nil) && (@entity.get_schedule != nil)
+    valid = (@entity != nil) && (@entity.get_schedule != nil) && (@date != nil)
 
     #check that if this is from cron, do not overwrite existing workout
     if @cause_of_new_routine == 'CRON'
@@ -95,6 +149,7 @@ class RoutineService
     @entity.current_phase = @phase_number
     @entity.save
 
+    #0 is a custom day, 99 is a Quadfit generated day with no weights
     if @routine.program_day_id == 0
       @routine.program_day_id = 99
     end
--- test/services/routine_service_test.rb
+++ test/services/routine_service_test.rb
@@ -8,7 +8,7 @@ class RoutineServiceTest < ActiveSupport::TestCase
 
   test 'should do nothing with nil user' do
     @user = User.new
-    @routine_service = RoutineService.new(@user, 'Cron', Date.today)
+    @routine_service = RoutineService.new(@user, 'Cron', Date.today, false)
     routine = @routine_service.create_routine
     assert(routine == nil)
   end
@@ -16,7 +16,7 @@ class RoutineServiceTest < ActiveSupport::TestCase
   test 'should do nothing with routine on date already' do
     date = Date.new(2014, 4, 26)
     @user = User.find(1)
-    @routine_service = RoutineService.new(@user, 'CRON', date)
+    @routine_service = RoutineService.new(@user, 'CRON', date, false)
     routine = @routine_service.create_routine
     assert(routine == nil)
   end
@@ -24,9 +24,65 @@ class RoutineServiceTest < ActiveSupport::TestCase
   test 'should create a routine' do
     date = Date.new(2014, 4, 27)
     @user = User.find(1)
-    @routine_service = RoutineService.new(@user, 'CRON', date)
+    @routine_service = RoutineService.new(@user, 'CRON', date, false)
     routine = @routine_service.create_routine
     assert(routine != nil)
+  end
+
+  test 'should return next stretching day for non sched update' do
+    date = Date.new(2014, 4, 26)
+    @user = User.find(2)
+    @routine_service = RoutineService.new(@user, 'CRON', date, false)
+    next_day = @routine_service.get_next_day_id(4)
+    assert(next_day == 2)
+  end
+
+  test 'should return next weights day for non sched update and wrap around' do
+    date = Date.new(2014, 4, 26)
+    @user = User.find(2)
+    @routine_service = RoutineService.new(@user, 'CRON', date, false)
+    next_day = @routine_service.get_next_day_id(2)
+    assert(next_day == 1)
+  end
+
+  test 'should return next plyos day for non sched update' do
+    date = Date.new(2014, 4, 26)
+    @user = User.find(2)
+    @routine_service = RoutineService.new(@user, 'CRON', date, false)
+    next_day = @routine_service.get_next_day_id(2)
+    assert(next_day == 1)
+  end
+
+  test 'should return next sprinting day for non sched update' do
+    date = Date.new(2014, 3, 26)
+    @user = User.find(2)
+    @routine_service = RoutineService.new(@user, 'CRON', date, false)
+    next_day = @routine_service.get_next_day_id(3)
+    assert(next_day == 2)
+  end
+
+  test 'should return next stretching day for sched update' do
+    date = Date.new(2014, 4, 26)
+    @user = User.find(2)
+    @routine_service = RoutineService.new(@user, 'CRON', date, true)
+    next_day = @routine_service.get_next_day_id(4)
+    assert(next_day == 1)
+  end
+
+  test 'should create routine with canned warm ups' do
+    date = Date.new(2014, 5, 18)
+    @user = User.find(1)
+    @routine_service = RoutineService.new(@user, 'CRON', date, false)
+    routine = @routine_service.create_routine
+    assert(routine.performed_warm_ups.size == 6)
+  end
+
+  test 'should create routine with warm ups from previous workout' do
+    date = Date.new(2014, 4, 27)
+    @user = User.find(1)
+    @routine_service = RoutineService.new(@user, 'CRON', date, false)
+    routine = @routine_service.create_routine
+    assert(routine.performed_warm_ups.size == 1)
   end
 
 end
