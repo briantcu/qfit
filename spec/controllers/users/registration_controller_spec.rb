b360b31266c5d94b066448c51d064d7dbb36d7bd
briantcu <bri.reg@gmail.com>
2016-04-17 17:44:11 -0700

    Registrations controller test

--- spec/controllers/users/registration_controller_spec.rb
+++ spec/controllers/users/registration_controller_spec.rb
@@ -2,28 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Users::RegistrationsController, type: :controller do
 
-# test 'should create sub user and tie to existing coach' do
-#   post(:create, user: { email: 'a@b.com', password: 'password', password_confirmation: 'password',
-#                         first_name: 'brian', last_name: 'regan', account_type: 'user', sign_up_code: 'MyString'})
-#   assert_response :created
-#   assert(@controller.instance_variable_get(:@user).id != nil)
-#   assert(@controller.instance_variable_get(:@user).level == 1)
-#   assert(@controller.instance_variable_get(:@user).password == 'password')
-#   assert(@controller.instance_variable_get(:@user).encrypted_password != 'password')
-#   assert(@controller.instance_variable_get(:@user).sub_user)
-#   assert(@controller.instance_variable_get(:@user).master_user_id == 1)
-# end
-#
-# test 'should make friend if signing up with a quad pod invite token' do
-#   invite = PodInvite.create!(inviter: User.last, sent_to: '8172913409')
-#   payload = {sent_to: invite.sent_to, id: invite.id}
-#   token = JWT.encode payload, Rails.application.config.token_salt, 'HS256'
-#   post(:create, user: {
-#       email: 'a@b.com', password: 'password', password_confirmation: 'password', first_name: 'brian',
-#       last_name: 'regan', account_type: 'user', sign_up_code: 'MyString'}, invite_token: token)
-#   assert_response :created
-#   assert(@controller.instance_variable_get(:@user).friends.count == 1)
-#   assert(@controller.instance_variable_get(:@user).phone == '8172913409')
-# end
+  before(:each) do
+    @controller = Users::RegistrationsController.new
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+
+    @coach = FactoryGirl.create(:user, level: 5)
+    @coach_account = FactoryGirl.create(:coach_account, user: @coach)
+    FactoryGirl.create(:sign_up_code, user: @coach, code: 'MyString')
+  end
+
+  it 'should create sub user and tie to existing coach' do
+    post(:create, user: { email: 'a@b.com', password: 'password', password_confirmation: 'password',
+                          first_name: 'brian', last_name: 'regan', account_type: 'user', sign_up_code: 'MyString'})
+    expect(response.status).to eq(201)
+    expect(@controller.instance_variable_get(:@user).id != nil).to be(true)
+    expect(@controller.instance_variable_get(:@user).level).to eq(1)
+    expect(@controller.instance_variable_get(:@user).password).to eq('password')
+    expect(@controller.instance_variable_get(:@user).encrypted_password != 'password').to be(true)
+    expect(@controller.instance_variable_get(:@user).sub_user).to be(true)
+    expect(@controller.instance_variable_get(:@user).master_user_id).to eq(@coach.id)
+  end
+
+  it 'should make friend if signing up with a quad pod invite token' do
+    invite = PodInvite.create!(inviter: User.last, sent_to: '8172913409')
+    payload = {sent_to: invite.sent_to, id: invite.id}
+    token = JWT.encode payload, Rails.application.config.token_salt, 'HS256'
+    post(:create, user: {
+        email: 'a@b.com', password: 'password', password_confirmation: 'password', first_name: 'brian',
+        last_name: 'regan', account_type: 'user', sign_up_code: 'MyString'}, invite_token: token)
+    expect(response.status).to eq(201)
+    expect(@controller.instance_variable_get(:@user).friends.count == 1).to be true
+    expect(@controller.instance_variable_get(:@user).phone == '8172913409').to be true
+  end
 
 end
