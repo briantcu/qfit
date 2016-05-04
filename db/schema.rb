29fa7d168dd8cd1b3a309a26d779cf4a92911cef
briantcu <bri.reg@gmail.com>
2016-05-03 22:44:46 -0700

    New exercises

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411011340) do
+ActiveRecord::Schema.define(version: 20160504021937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20160411011340) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "paid_tier",                    default: 1
+    t.boolean  "for_time"
   end
 
   create_table "faqs", force: :cascade do |t|
