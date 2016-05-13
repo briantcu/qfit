c82091eecb3bfff3d8ed9c7ebb2f75093f8202f0
briantcu <bri.reg@gmail.com>
2016-05-12 22:48:03 -0700

    floatal

--- app/assets/javascripts/views/common/fancy_input.jsx
+++ app/assets/javascripts/views/common/fancy_input.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import {render} from 'react-dom';
+
+import Floatl from 'floatl';
+var $ = require("jQuery");
 require('fancy_input.scss');
 
 class FancyInput extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        new Floatl(this.refs.fancyInput);
+    }
+
     render () {
-        return <input type="text" className="transparent-input form-text" />;
+        return <span ref="fancyInput" className="floatl">
+                    <label className="floatl__label">Name</label>
+                    <input type="text" className="transparent-input form-text floatl__input" placeholder="Name" />
+            </span>;
     }
 }
 export default FancyInput;
