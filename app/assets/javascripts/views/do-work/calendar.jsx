e82478df019a5d2dd37b21ba2628e515078e786b
briantcu <bri.reg@gmail.com>
2016-06-29 21:31:21 -0700

    Calendar loading

--- app/assets/javascripts/views/do-work/calendar.jsx
+++ app/assets/javascripts/views/do-work/calendar.jsx
@@ -8,10 +8,11 @@ class CalendarCell extends React.Component {
     }
 
     render() {
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         return <div className="col-xs-2 calendar-cell">
-            <div className="cal-subtext">Monday</div>
-            <div className="cal-text">April, 4th</div>
-            <div className="cal-subtext">Workout</div>
+            <div className="cal-subtext">{days[this.props.day.day_of_week]}</div>
+            <div className="cal-text">{this.props.day.day_of_month}</div>
+            <div className="cal-subtext">{this.props.day.workout_status}</div>
         </div>
     }
 }
@@ -19,18 +20,42 @@ class CalendarCell extends React.Component {
 class Calendar extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loaded: false,
+            daysToShow: []
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.getRowToShow(nextProps);
+    }
+
+    getRowToShow(props) {
+        var daysToShow = [];
+        var index = 0;
+        if (props.calendar && props.calendar.attributes && !this.state.loaded) {
+            var days = props.calendar.attributes.calendar_month.days;
+            var day = props.day;
+            _.each(days, function (elem, indx, list) {
+                if (elem.day_of_month == day) {
+                    index = indx;
+                    return;
+                }
+            });
+            daysToShow = days.slice(index, index + 5);
+            this.setState({loaded: true, daysToShow: daysToShow});
+        }
     }
 
     render() {
         return <div className="row calendar">
             <div className="container">
                 <div className="row">
-                    <CalendarCell />
-                    <CalendarCell />
-                    <CalendarCell />
-                    <CalendarCell />
-                    <CalendarCell />
-                    <CalendarCell />
+                    {
+                        this.state.daysToShow.map(function(e, index) {
+                            return <CalendarCell day={e} key={e.day_of_month}/>
+                        })
+                    }
                 </div>
             </div>
 
