619af1aea601184fca833179a6f65aa57318793e
briantcu <bri.reg@gmail.com>
2016-09-04 14:20:04 -0700

    Leaders

--- app/assets/javascripts/views/leaders/leader.jsx
+++ app/assets/javascripts/views/leaders/leader.jsx
@@ -10,13 +10,13 @@ class Leader extends React.Component {
 
     render () {
         return <div className="leader row">
-            <div className="col-xs-8">
+            <div className="col-xs-8 pad-override">
                 <Avatar user={this.props.leader} />
                 <div className="username">
                     {this.props.leader.user_name}
                 </div>
             </div>
-            <div className="value col-xs-4">{this.props.leader.value}</div>
+            <div className="value col-xs-4 pad-override">{this.props.val}</div>
         </div>;
     }
 }
