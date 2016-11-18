f3311ab50c69cf198259b663e04282498bcdf341
briantcu <bri.reg@gmail.com>
2016-11-17 22:07:16 -0800

    New redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 if Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://quadfit-dev.89gbzf.0001.use1.cache.amazonaws.com:6379/12' }
+    config.redis = { url: 'redis://quadfit.t9e3qk.0001.usw1.cache.amazonaws.com:6379/12' }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://quadfit-dev.89gbzf.0001.use1.cache.amazonaws.com:6379/12' }
+    config.redis = { url: 'redis://quadfit.t9e3qk.0001.usw1.cache.amazonaws.com:6379/12' }
   end
 end
