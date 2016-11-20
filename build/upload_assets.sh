3704cf02f1af754e010eeca5200be47524554cb2
briantcu <bri.reg@gmail.com>
2016-11-20 14:47:41 -0800

    Start config and fix asset upload

--- build/upload_assets.sh
+++ build/upload_assets.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-aws s3 cp "public/js/" s3://quadfit/assets/ --recursive --acl public-read
-aws s3 cp "public/css/" s3://quadfit/assets/ --recursive --acl public-read
+aws s3 cp "public/js/" s3://quadfit/assets/js/ --recursive --acl public-read
+aws s3 cp "public/css/" s3://quadfit/assets/css/ --recursive --acl public-read
