05a8085b1ba8ce8c960d7cce000a22fc9168317f
briantcu <bri.reg@gmail.com>
2016-11-20 13:29:14 -0800

    Move to rspec

--- spec/controllers/users/registration_controller_spec.rb
+++ spec/controllers/users/registration_controller_spec.rb
@@ -36,4 +36,56 @@ RSpec.describe Users::RegistrationsController, type: :controller do
     expect(@controller.instance_variable_get(:@user).phone == '8172913409').to be true
   end
 
+  it 'should create regular user account passing in empty sign up code' do
+
+    post(:create, user: { email: 'a@b.com', password: 'password', password_confirmation: 'password',
+                          first_name: 'brian', last_name: 'regan', sign_up_code: '', account_type: 'user'})
+    expect(response.status).to eq(201)
+    assert(@controller.instance_variable_get(:@user).id != nil)
+    assert(@controller.instance_variable_get(:@user).level == 2)
+    assert(@controller.instance_variable_get(:@user).password == 'password')
+    assert(@controller.instance_variable_get(:@user).encrypted_password != 'password')
+    assert(!@controller.instance_variable_get(:@user).sub_user)
+  end
+
+  it 'should create regular user account without passing in sign up code' do
+    post(:create, user: { email: 'a@b.com', password: 'password', password_confirmation: 'password',
+                          first_name: 'brian', last_name: 'regan', account_type: 'user'})
+    assert_response :created
+    assert(@controller.instance_variable_get(:@user).id != nil)
+    assert(@controller.instance_variable_get(:@user).level == 2)
+    assert(@controller.instance_variable_get(:@user).password == 'password')
+    assert(@controller.instance_variable_get(:@user).encrypted_password != 'password')
+    assert(!@controller.instance_variable_get(:@user).sub_user)
+  end
+
+  it 'should not create regular user with invalid email' do
+    post(:create, user: { email: 'b.com', password: 'password', password_confirmation: 'password',
+                          first_name: 'brian', last_name: 'regan', account_type: 'user'})
+    expect(response.status).to eq(422)
+  end
+
+  it 'should not create regular user with email that exists' do
+    post(:create, user: { email: 'MyString6@a.com', password: 'password', password_confirmation: 'password',
+                          first_name: 'brian', last_name: 'regan', account_type: 'user'})
+    expect(response.status).to eq(201)
+  end
+
+  it 'should not create user account with invalid sign up code' do
+    post(:create, user: { email: 'a@b.com', password: 'password', password_confirmation: 'password',
+                          first_name: 'brian', last_name: 'regan', sign_up_code: 'code', account_type: 'user'})
+    expect(response.status).to eq(470)
+  end
+
+  it 'should create coach account' do
+    post(:create, user: { email: 'a@b.com', password: 'password', password_confirmation: 'password',
+                          first_name: 'brian', last_name: 'regan', account_type: 'coach'})
+    expect(response.status).to eq(201)
+    assert(@controller.instance_variable_get(:@user).id != nil)
+    assert(@controller.instance_variable_get(:@user).level == 5)
+    assert(@controller.instance_variable_get(:@user).password == 'password')
+    assert(@controller.instance_variable_get(:@user).encrypted_password != 'password')
+    assert(!@controller.instance_variable_get(:@user).sub_user)
+  end
+
 end
