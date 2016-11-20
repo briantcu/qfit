b22dcd5e3568bea9f1bc7f45c944fadeaa7e8611
briantcu <bri.reg@gmail.com>
2016-11-20 15:21:06 -0800

    Minify

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,13 @@ var config = {
         }),
         new webpack.SourceMapDevToolPlugin({
             filename: '[file].map'
-        })
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
     ]
 };
 
