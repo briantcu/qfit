797d9c13619822fe917ddb288c5034765bc62d77
briantcu <bri.reg@gmail.com>
2016-11-20 13:58:28 -0800

    set asset prefix in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Qfit::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets_prefix = ''
+  config.assets_prefix = 'https://s3.amazonaws.com/quadfit/assets/'
 
   # Generate digests for assets URLs.
   config.assets.digest = true
