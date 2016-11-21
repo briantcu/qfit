931883d3e565eda933be0b0508c6d8544519bdd4
briantcu <bri.reg@gmail.com>
2016-11-20 19:48:35 -0800

    Fix

--- build/upload_assets.sh
+++ build/upload_assets.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 BUILD_NUM=$1
-aws s3 cp "public/js/" s3://quadfit/assets/js/${BUILD_NUM}/ --recursive --acl public-read
-aws s3 cp "public/css/" s3://quadfit/assets/css/${BUILD_NUM}/ --recursive --acl public-read
+aws s3 cp "public/js/" s3://quadfit/assets/${BUILD_NUM}/js/ --recursive --acl public-read
+aws s3 cp "public/css/" s3://quadfit/assets/${BUILD_NUM}/css/ --recursive --acl public-read
