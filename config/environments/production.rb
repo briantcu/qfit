506d2f6722238712536802fac3dbcfc0c2bddcde
briantcu <bri.reg@gmail.com>
2016-11-26 18:01:15 -0800

    emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Qfit::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "www.quadfit.com" }
+  config.action_mailer.default_url_options = { :host => "quadfit-env.us-west-1.elasticbeanstalk.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address => "email-smtp.us-east-1.amazonaws.com",
